c9e7aa92f729fe5630e9e4efd83d13e08f63a4be
DiegoSZ675 <diego.asanchezz08@gmail.com>
2026-05-27 01:10:13 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$base_url = "http://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+if (!isset($_SESSION['id_usuario'])) {
+    // Te manda al login usando la ruta completa
+    header("Location: " . $base_url . "/php/endpoints/login.php");
+    exit();
+} else {
+    // Te manda al panel usando la ruta completa
+    header("Location: " . $base_url . "/frondend/html/index.php");
+    exit();
+}
+?>
